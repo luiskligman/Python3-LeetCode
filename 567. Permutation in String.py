45e880fcdb663e2b1d6464ac34b0e07112492c2c
Luis Kligman <137526222+luiskligman@users.noreply.github.com>
2026-07-16 12:05:46 -0400

    Enhance code readability with comments
    Added comments to explain character counting and sliding window logic.

--- 567. Permutation in String.py
+++ 567. Permutation in String.py
@@ -2,19 +2,23 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2): return False
 
-        s1count, s2count = [0] * 26, [0] * 26
+        s1count, s2count = [0] * 26, [0] * 26  # Create two empty lists to count character occurances
         for i in range(len(s1)):
             s1count[ord(s1[i]) - ord('a')] += 1
             s2count[ord(s2[i]) - ord('a')] += 1
         
         matches = 0
         for i in range(26):
+            # compare each of the 26 char counts, we could have a permutation at this point
             matches += (1 if s1count[i] == s2count[i] else 0)
 
+        # Have a sliding window l and r, add r to count and remove l from count
         l = 0
         for r in range(len(s1), len(s2)):
             if matches == 26: return True
+            # check for full match each time
 
+            # check if adding s2[r] creates a match
             index = ord(s2[r]) - ord('a')
             s2count[index] += 1
             if s1count[index] == s2count[index]:
@@ -22,6 +26,7 @@ class Solution:
             elif s1count[index] + 1 == s2count[index]:
                 matches -= 1
 
+            # check if removing s2[l] creates a match
             index = ord(s2[l]) - ord('a')
             s2count[index] -= 1
             if s1count[index] == s2count[index]:
@@ -29,5 +34,6 @@ class Solution:
             elif s1count[index] - 1 == s2count[index]:
                 matches -= 1
 
+            # iterate l each time due to properties of sliding window approach
             l += 1
         return matches == 26
